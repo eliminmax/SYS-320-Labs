3753e381573c4ebfa5811c1429fa9dfc8b1c8a40
Eli Array Minkoff <eli.minkoff@mymail.champlain.edu>
2022-02-12 19:50:20 -0500

    Add scanner.py, remove fs.py

---
+++ Week04/homework/scanner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"Scan logs in directory for indications of various attack types"
+import yaml
+import argparse
+import re
+import yaml
+
+import fs
+
+
+def load_search_patterns(filepath):
+    pass
